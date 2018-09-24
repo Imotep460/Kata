fc85c991cebfef01086399dbe065667727c5bebe
Hasse Augustinussen <hasse_augu@hotmail.dk>
2018-09-24 12:36:03 +0200

    creation + setup first test

---
+++ MyTest/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("MyTest")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("MyTest")]
+[assembly: AssemblyCopyright("Copyright ©  2018")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("752bba25-acec-4d5b-ae9c-0157cc5fba2b")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ MyTest/UnitTest1.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace MyTest
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void FirstTest()
+        {
+            List<int[]> = peopleList = new List<int[]>()
+            {
+                new []{10,0},
+                new []{3,5},
+                new []{5,8}
+            };
+            Assert.AreEqual(5, Kata.Number(peopleList));
+
+        }
+    }
+}
