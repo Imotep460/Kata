91f9dcc2d9773b4a8246b3b1534e2f420ae0378d
Hasse Augustinussen <hasse_augu@hotmail.dk>
2018-10-25 12:33:24 +0200

    test setup complete.

--- TestKata/UnitTest1.cs
+++ TestKata/UnitTest1.cs
@@ -1,26 +1,36 @@
 ﻿using System;
+using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
 namespace TestKata
 {
+    
     [TestClass]
     public class UnitTest1
     {
+        private int Number(List<int[]> peopleList)
+        {
+            throw new NotImplementedException();
+        }
+
         [TestMethod]
         public void FirstTest()
         {
-            List<int[]> = peopleList = new List<int[]>()
+            List<int[]> peopleList = new List<int[]>()
             {
                 new []{10,0},
                 new []{3,5},
                 new []{5,8}
             };
-            Assert.AreEqual(5, Kata.Number(peopleList));
+            Assert.AreEqual(5, Number(peopleList));
         }
+
+        
+
         [TestMethod]
         public void SecondTest()
         {
-            List<int[]> = peopleList = new List<int[]>()
+            List<int[]> peopleList = new List<int[]>()
             {
                 new []{3,0},
                 new []{9,1},
@@ -29,12 +39,12 @@ namespace TestKata
                 new []{6,1},
                 new []{7,10}
             };
-            Assert.AreEqual(17, Kata.Number(peopleList));
+            Assert.AreEqual(17, Number(peopleList));
         }
         [TestMethod]
         public void ThridTest()
         {
-            List<int[]> = peopleList = new List<int[]>()
+            List<int[]> peopleList = new List<int[]>()
             {
                 new []{3,0},
                 new []{9,1},
@@ -43,7 +53,7 @@ namespace TestKata
                 new []{6,1},
                 new []{7,8}
             };
-            Assert.AreEqual(21, Kata.Number(peopleList));
+            Assert.AreEqual(21, Number(peopleList));
         }
     }
 }
