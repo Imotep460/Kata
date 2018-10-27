2ec4dc0b7c0c7e2c9abccd0ad7142d82907e7e70
Hasse Augustinussen <hasse_augu@hotmail.dk>
2018-10-27 16:57:22 +0200

    descriptions and comments added.

--- TestKata/UnitTest1.cs
+++ TestKata/UnitTest1.cs
@@ -3,6 +3,21 @@ using System.Collections.Generic;
 using System.Linq;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
+// Kata description
+//Number of people in the bus
+//There is a bus moving in the city, and it takes and drop some people in each bus stop.
+
+//You are provided with a list(or array) of integer arrays(or tuples). Each integer array has two items which represent number of people get into bus(The first item) and number of people get off the bus(The second item) in a bus stop.
+
+//Your task is to return number of people who are still in the bus after the last bus station(after the last array). Even though it is the last bus stop, the bus is not empty and some people are still in the bus, and they are probably sleeping there :D
+
+//Take a look on the test cases.
+
+//Please keep in mind that the test cases ensure that the number of people in the bus is always >= 0. So the return integer can't be negative.
+
+
+//The second value in the first integer array is 0, since the bus is empty in the first bus stop.
+
 namespace TestKata
 {
 
@@ -15,6 +30,8 @@ namespace TestKata
         //{
         //    return peopleListInOut.Sum(Item => Item[0] - Item[1]);
         //}
+
+        // Note: that the Linq solution does not exclude negative integers.
 
         static public int counter = 0;
         static int[] busStop = new int[] { };
@@ -30,6 +47,8 @@ namespace TestKata
             }
             return counter;
         }
+
+        // Note that the solution above does not exclude negative integers.
 
         [TestMethod]
         public void FirstTest()
