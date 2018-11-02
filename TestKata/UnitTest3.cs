c97c7a5658c02bd41e785180debbf75f4c7f9871
Hasse Augustinussen <hasse_augu@hotmail.dk>
2018-11-02 14:21:21 +0100

    ...

--- TestKata/UnitTest3.cs
+++ TestKata/UnitTest3.cs
@@ -2,48 +2,50 @@
 using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.ValueTuple;
 using System.Linq;
 
-//namespace TestKata
-//{
-////In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
-
-////Example:
-
-////Kata.HighAndLow("1 2 3 4 5"); // return "5 1"
-////    Kata.HighAndLow("1 2 -3 4 5"); // return "5 -3"
-////    Kata.HighAndLow("1 9 3 4 -5"); // return "9 -5"
-////    Notes:
-
-////All numbers are valid Int32, no need to validate them.
-////There will always be at least one number in the input string.
-////Output string must be two numbers separated by a single space, and highest number is first.
-
-[TestClass]
-public class UnitTest3
+namespace TestKata
 {
-    public static string HighAndLow(string Numbers)
+    ////In this little assignment you are given a string of space separated numbers, and have to return the highest and lowest number.
+
+    ////Example:
+
+    ////Kata.HighAndLow("1 2 3 4 5"); // return "5 1"
+    ////    Kata.HighAndLow("1 2 -3 4 5"); // return "5 -3"
+    ////    Kata.HighAndLow("1 9 3 4 -5"); // return "9 -5"
+    ////    Notes:
+
+    ////All numbers are valid Int32, no need to validate them.
+    ////There will always be at least one number in the input string.
+    ////Output string must be two numbers separated by a single space, and highest number is first.
+
+    [TestClass]
+    public class UnitTest3
     {
-        string[] splittedLine = Numbers.Split(' ');
-
-        int max = Int32.MinValue;
-        int min = Int32.MaxValue;
-
-        foreach (var number in splittedLine)
+        public static string HighAndLow(string Numbers)
         {
-            int currentNumber = int.Parse(number);
+            string[] splittedLine = Numbers.Split(' ');
 
-            if (currentNumber > max)
-                max = currentNumber;
-            if (currentNumber < min)
-                min = currentNumber;
+            int max = Int32.MinValue;
+            int min = Int32.MaxValue;
+
+            foreach (var number in splittedLine)
+            {
+                int currentNumber = int.Parse(number);
+
+                if (currentNumber > max)
+                    max = currentNumber;
+                if (currentNumber < min)
+                    min = currentNumber;
+            }
+            return ToString();
         }
-        return (max, min));
-    }
 
-    [TestMethod]
-    public void TestMethod1()
-    {
-        Assert.AreEqual("42 -9", HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+        [TestMethod]
+        public void TestMethod1()
+        {
+            Assert.AreEqual("42 -9", HighAndLow("8 3 -5 42 -1 0 0 -9 4 7 4 -4"));
+        }
     }
 }
