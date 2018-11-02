407f4bd9fff151c38f005418dfaf14ad3ab30e6d
Hasse Augustinussen <hasse_augu@hotmail.dk>
2018-11-03 00:57:30 +0100

    comments fix

--- TestKata/Unittests/UniqueChars.cs
+++ TestKata/Unittests/UniqueChars.cs
@@ -13,8 +13,7 @@ namespace TestKata
         //Write a program to determine if a string contains all unique characters.Return true if it does and false otherwise.
         //The string may contain any of the 128 ASCII characters.
 
-
-        ////Super simple1:
+        ////Super simple 1:
         //public static bool HasUniqueChars(string str)
         //{
         //    return str.Distinct().SequenceEqual(str);
