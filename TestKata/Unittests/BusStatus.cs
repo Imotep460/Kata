60d3792d4d481d81ec1bd0aec536bf0d0c9a8e42
Hasse Augustinussen <hasse_augu@hotmail.dk>
2019-06-03 12:58:10 +0200

    refactor

--- TestKata/Unittests/BusStatus.cs
+++ TestKata/Unittests/BusStatus.cs
@@ -24,18 +24,17 @@ namespace TestKata
     [TestClass]
     public class BusStatus
     {
-        ////Linq sulution:
-
-        //public static int Number(List<int[]> peopleListInOut)
-        //{
-        //    return peopleListInOut.Sum(Item => Item[0] - Item[1]);
-        //}
+        //Linq sulution:
+        public static int Number0(List<int[]> peopleListInOut)
+        {
+            return peopleListInOut.Sum(Item => Item[0] - Item[1]);
+        }
 
         // Note: that the Linq solution does not exclude negative integers.
 
         static public int counter = 0;
         static int[] busStop = new int[] { };
-        private int Number(List<int[]> peopleListInOut)
+        private int Number1(List<int[]> peopleListInOut)
         {
             counter = 0;
             for (int i = 0; i < peopleListInOut.Count; i++)
@@ -59,7 +58,8 @@ namespace TestKata
                 new []{3,5},
                 new []{5,8}
             };
-            Assert.AreEqual(5, Number(peopleList));
+            Assert.AreEqual(5, Number0(peopleList));
+            Assert.AreEqual(5, Number1(peopleList));
         }
 
         
@@ -76,7 +76,8 @@ namespace TestKata
                 new []{6,1},
                 new []{7,10}
             };
-            Assert.AreEqual(17, Number(peopleList));
+            Assert.AreEqual(17, Number0(peopleList));
+            Assert.AreEqual(17, Number1(peopleList));
         }
         [TestMethod]
         public void ThridTest()
@@ -90,7 +91,8 @@ namespace TestKata
                 new []{6,1},
                 new []{7,8}
             };
-            Assert.AreEqual(21, Number(peopleList));
+            Assert.AreEqual(21, Number0(peopleList));
+            Assert.AreEqual(21, Number1(peopleList));
         }
     }
 }
