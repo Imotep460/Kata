6f78135dacfc115379c27c4d96e2e61d2a0f9129
Hasse Augustinussen <hasse_augu@hotmail.dk>
2019-06-03 12:54:38 +0200

    refactor

--- TestKata/Unittests/UniqueChars.cs
+++ TestKata/Unittests/UniqueChars.cs
@@ -2,6 +2,7 @@
 using System.Text;
 using System.Collections.Generic;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System.Linq;
 
 namespace TestKata
 {
@@ -12,53 +13,53 @@ namespace TestKata
         //Write a program to determine if a string contains all unique characters.Return true if it does and false otherwise.
         //The string may contain any of the 128 ASCII characters.
 
-        ////Super simple 1:
-        //public static bool HasUniqueChars(string str)
-        //{
-        //    return str.Distinct().SequenceEqual(str);
-        //}
+        //Super simple 1:
+        public static bool HasUniqueChars0(string str)
+        {
+            return str.Distinct().SequenceEqual(str);
+        }
 
-        ////Super simple 2:
-        //public static bool HasUniqueChars(string str)
-        //{
-        //    return str.ToArray().Distinct().Count() == str.Length;
-        //}
+        //Super simple 2:
+        public static bool HasUniqueChars1(string str)
+        {
+            return str.ToArray().Distinct().Count() == str.Length;
+        }
 
-        //// linq solution 1:
+        // linq solution 1:
 
-        //public static bool HasUniqueChars(string str)
-        //{
-        //    return str.All(n => str.Count(m => m == n) == 1);
-        //}
+        public static bool HasUniqueChars2(string str)
+        {
+            return str.All(n => str.Count(m => m == n) == 1);
+        }
 
-        //// Linq Solution 2:
-        //public static bool HasUniqueChars(string str)
-        //{
-        //    var chars = (str.GroupBy(n => n)
-        //              .Select(n => n.Count())).Count();
+        // Linq Solution 2:
+        public static bool HasUniqueChars3(string str)
+        {
+            var chars = (str.GroupBy(n => n)
+                      .Select(n => n.Count())).Count();
 
-        //    return (chars == str.Length);
-        //}
+            return (chars == str.Length);
+        }
 
-        //// Linq Solution 3:
-        //public static bool HasUniqueChars(string str) => string.Concat(str.Distinct()) == str;
+        // Linq Solution 3:
+        public static bool HasUniqueChars4(string str) => string.Concat(str.Distinct()) == str;
 
-        ////Linq solution 4:
-        //public static bool HasUniqueChars(string str)
-        //{
-        //    var grouped = str.GroupBy(n => n).Select(l => new { Value = l.Key, Count = l.Count() }).ToList();
+        //Linq solution 4:
+        public static bool HasUniqueChars5(string str)
+        {
+            var grouped = str.GroupBy(n => n).Select(l => new { Value = l.Key, Count = l.Count() }).ToList();
 
-        //    foreach (var letter in grouped)
-        //    {
-        //        if (letter.Count > 1)
-        //        {
-        //            return false;
-        //        }
-        //    }
-        //    return true;
-        //}
+            foreach (var letter in grouped)
+            {
+                if (letter.Count > 1)
+                {
+                    return false;
+                }
+            }
+            return true;
+        }
 
-        public static bool HasUniqueChars(string str)
+        public static bool HasUniqueChars6(string str)
         {
             if (str.Length > 256)
             {
@@ -80,31 +81,61 @@ namespace TestKata
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.IsTrue(HasUniqueChars("abcdef"));
+            Assert.IsTrue(HasUniqueChars0("abcdef"));
         }
 
         [TestMethod]
         public void TestMethod2()
         {
-            Assert.IsFalse(HasUniqueChars("++-"));
+            Assert.IsFalse(HasUniqueChars1("++-"));
         }
 
         [TestMethod]
         public void TestMethod3()
         {
-            Assert.IsFalse(HasUniqueChars("  nAa"));
+            Assert.IsFalse(HasUniqueChars2("  nAa"));
         }
 
         [TestMethod]
         public void TestMethod4()
         {
-            Assert.IsFalse(HasUniqueChars("aba"));
+            Assert.IsFalse(HasUniqueChars3("aba"));
         }
 
         [TestMethod]
         public void TestMethod5()
         {
-            Assert.IsTrue(HasUniqueChars("Is True"));
+            Assert.IsTrue(HasUniqueChars4("Is True"));
+        }
+
+        [TestMethod]
+        public void TestMethod6()
+        {
+            Assert.IsTrue(HasUniqueChars5("abcdef"));
+        }
+
+        [TestMethod]
+        public void TestMethod7()
+        {
+            Assert.IsFalse(HasUniqueChars6("++-"));
+        }
+
+        [TestMethod]
+        public void TestMethod8()
+        {
+            Assert.IsFalse(HasUniqueChars0("  nAa"));
+        }
+
+        [TestMethod]
+        public void TestMethod9()
+        {
+            Assert.IsFalse(HasUniqueChars1("aba"));
+        }
+
+        [TestMethod]
+        public void TestMethod10()
+        {
+            Assert.IsTrue(HasUniqueChars2("Is True"));
         }
     }
 }
