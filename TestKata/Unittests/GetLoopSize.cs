50b0b4b40e2d8f7dd5860c1fda3d60624103a9e6
Hasse Augustinussen <hasse_augu@hotmail.dk>
2019-10-17 15:33:03 +0200

    Added new Kata GetLoopSize.cs

---
+++ TestKata/Unittests/GetLoopSize.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Text;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace TestKata.Unittests
+{
+    /// <summary>
+    /// Summary description for GetLoopSize
+    /// </summary>
+    [TestClass]
+    public class GetLoopSize
+    {
+        public GetLoopSize()
+        {
+            //
+            // TODO: Add constructor logic here
+            //
+        }
+
+        [TestMethod]
+        public void TestMethod1()
+        {
+            //
+            // TODO: Add test logic here
+            //
+        }
+    }
+}
